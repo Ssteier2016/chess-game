87eaa979ae439e466cfc05be93ca3aa5d06f5e0b
Ssteier2016 <109993596+Ssteier2016@users.noreply.github.com>
2025-03-30 20:21:32 -0300

    install_stockfish.sh
    "Add script to install Stockfish".

---
+++ install_stockfish.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Descarga Stockfish 16 (ajustá la versión si querés)
+curl -Lo /opt/render/project/src/stockfish https://github.com/official-stockfish/Stockfish/releases/download/sf_16/stockfish-ubuntu-x86-64-modern
+# Dale permisos de ejecución
+chmod +x /opt/render/project/src/stockfish
